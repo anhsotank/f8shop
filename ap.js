f4f4eeb6e38f404bf338eb821492b488cb16987e
anhsotank <117705069+anhsotank@users.noreply.github.com>
2023-01-19 19:32:15 +0700

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,25 +1,80 @@
-$(document).ready(function(){
-    $(".slide_image").slick({
-        slidesToShow: 4,
-        slidesToScroll:1,
-        dots: true,
-        arrows:false,
-        autoplay: true,
-        autoplaySpeed: 1500,
 
-    });
-  });
   $(document).ready(function(){
-    $(".paginattion").slick({
-        slidesToShow: 3,
-        arrows:true,
-        slidesToScroll:1,
-        infinite:false,
-        prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='bx bxs-left-arrow-circle'></i></button>",
-            nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='bx bxs-right-arrow-circle'></i></button>"
-
-    });
+      $(".slide_image").slick({
+          slidesToShow: 4,
+          slidesToScroll:1,
+          dots: true,
+          arrows:false,
+          autoplay: true,
+          autoplaySpeed: 1500,
+  
+      });
+   });
+   $(document).ready(function(){
+      $(".slide_image1").slick({
+          slidesToShow: 2,
+          slidesToScroll:1,
+          dots: true,
+          arrows:false,
+          autoplay: true,
+          autoplaySpeed: 1500,
+  
+      });
   });
+
+  //chuyen trang
+  let thispage=1
+  let limit=20
+  let list=document.querySelectorAll('.movie-list .movie-item')
+  function loaditem(){
+    let beginpage=limit  *(thispage - 1)
+    let endpage=limit * thispage - 1
+    list.forEach((item,key)=>{
+      if(key>=beginpage && key<=endpage){
+        item.style.display='block'
+      }else{
+        item.style.display='none'
+      }
+    })
+    listpage();
+  }
+  loaditem();
+  function listpage(){
+    let cout=Math.ceil(list.length / limit);
+    document.querySelector('.paginattion').innerHTML='';
+    if(thispage!=1){
+      let first= document.createElement('li');
+        first.innerText='First';
+        first.setAttribute('onclick',"changepage("+ 1 +")")
+        document.querySelector('.paginattion').appendChild(first)
+        let prev= document.createElement('li');
+          prev.innerText='Prev';
+          prev.setAttribute('onclick',"changepage("+ (thispage-1) +")")
+          document.querySelector('.paginattion').appendChild(prev)
+    }
+    
+    for(i=1;i<=cout;i++){
+      let newpage = document.createElement('li')
+      newpage.innerText=i;
+      if(i==thispage){
+        newpage.classList.add('active');
+
+      }
+      newpage.setAttribute('onclick',"changepage("+ i +")")
+      document.querySelector('.paginattion').appendChild(newpage)
+    }
+    if(thispage!=cout){
+      let next= document.createElement('li');
+        next.innerText='Next';
+        next.setAttribute('onclick',"changepage("+ (thispage + 1) +")")
+        document.querySelector('.paginattion').appendChild(next)
+    }
+  }
+  function changepage(i){
+    thispage=i;
+    loaditem();
+
+  }
   //wifi
   let alerts = document.querySelectorAll('.alert');
     alerts.forEach(item=>{
